6fc9cc9912767ce439c224ac60d29399e06d09ac
Isaac Karth <isaac@isaackarth.com>
2021-04-12 19:05:55 -0700

    cleaning up unneaded comments in view

--- src/main/ecological/views.cljs
+++ src/main/ecological/views.cljs
@@ -115,40 +115,6 @@
                     [(hex-string-to-byte h-val)]))))
 
 
-;;       for (let yi = 0; yi < height; yi++) {
-;;         for (let xi = 0; xi < width; xi++) {
-;;           const collisionIndex = width * yi + xi;
-;;           const tile = collisions[collisionIndex];
-;;           if ((tile & COLLISION_ALL) === COLLISION_ALL) {
-;;             ctx.fillStyle = "rgba(250,40,40,0.6)";
-;;             ctx.fillRect(xi * TILE_SIZE, yi * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-;;           } else if (tile !== 0) {
-;;             if (tile & COLLISION_TOP) {
-;;               ctx.fillStyle = "rgba(40,40,250,0.6)";
-;;               ctx.fillRect(xi * TILE_SIZE, yi * TILE_SIZE, TILE_SIZE, TILE_SIZE * 0.4);
-;;             }
-;;             if (tile & COLLISION_BOTTOM) {
-;;               ctx.fillStyle = "rgba(255,250,40,0.6)";
-;;               ctx.fillRect(xi * TILE_SIZE, (yi + 0.6) * TILE_SIZE, TILE_SIZE, TILE_SIZE * 0.4);
-;;             }
-;;             if (tile & COLLISION_LEFT) {
-;;               ctx.fillStyle = "rgba(250,40,250,0.6)";
-;;               ctx.fillRect(xi * TILE_SIZE, yi * TILE_SIZE, TILE_SIZE * 0.4, TILE_SIZE);
-;;             }
-;;             if (tile & COLLISION_RIGHT) {
-;;               ctx.fillStyle = "rgba(40,250,250,0.6)";
-;;               ctx.fillRect((xi + 0.6) * TILE_SIZE, yi * TILE_SIZE, TILE_SIZE * 0.4, TILE_SIZE);
-;;             }                   
-;;           }
-;;           if (tile & TILE_PROP_LADDER) {
-;;             ctx.fillStyle = "rgba(0,128,0,0.6)";
-;;             ctx.fillRect((xi + 0.0) * TILE_SIZE, yi * TILE_SIZE, TILE_SIZE * 0.2, TILE_SIZE);
-;;             ctx.fillRect((xi + 0.8) * TILE_SIZE, yi * TILE_SIZE, TILE_SIZE * 0.2, TILE_SIZE);
-;;             ctx.fillRect(xi * TILE_SIZE, (yi + 0.4) * TILE_SIZE, TILE_SIZE, TILE_SIZE * 0.2);
-;;           }             
-;;         }
-;;       }
-
 ;; https://github.com/chrismaltby/gb-studio/blob/9cc3b4d341a6db6a6c10f7f35fb7f60e969f1d8c/src/consts.js#L50
 ;; export const COLLISION_TOP = 0x1;
 ;; export const COLLISION_BOTTOM = 0x2;
