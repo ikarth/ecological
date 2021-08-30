f6c625bdd079d7bd94872946672554bb71181a1d
Isaac Karth <isaac@isaackarth.com>
2021-08-30 12:41:09 -0700

    remove old gbs generator

--- src/main/ecological/generator/gbs-moves.cljs
+++
@@ -1,8 +0,0 @@
-(ns ecological.generator.gbs-moves
-  (:require [datascript.core :as d]
-            [clojure.string]
-            ;[goog.crypt :as crypt]
-            ;[quil.core :as qc]
-            ;[quil.middleware :as qm]
-            ;[quil.applet :as qa]
-            ))
